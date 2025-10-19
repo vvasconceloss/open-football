f528ca2a92400ee8367ba43a7a4b245979d74f4a
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-19 11:12:49 +0100

    feat(domain): create a value object to represent the positions of the player entity

---
+++ src/domain/src/value_objects/player/position.rs
@@ -0,0 +1,18 @@
+#[derive(Debug, Clone)]
+pub enum Position {
+    GK,  // Goalkeeper
+    RB,  // Right Back
+    CB,  // Center Back
+    LB,  // Left Back
+    RWB, // Right Wing-back
+    LWB, // Left Wing-back
+    DM,  // Defensive Midfielder
+    CM,  // Center Midfielder
+    AM,  // Attack Midfielder
+    LM,  // Left Midfielder
+    RM,  // Right Midfielder
+    LW,  // Left Winger
+    RW,  // Right Winger
+    SS,  // Second Striker
+    ST,  // Striker
+}
