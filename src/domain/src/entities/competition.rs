659c9f235d9815cb0530f3d2c927415d487246c2
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-21 16:10:59 +0100

    refactor(domain): refactor competition entity (add getters, VOs, and behaviour methods)

--- src/domain/src/entities/competition.rs
+++ src/domain/src/entities/competition.rs
@@ -1,6 +1,9 @@
 use crate::{
     entities::{club::ClubId, fixture::FixtureId},
-    value_objects::competitions::standing::Standing,
+    errors::DomainError,
+    value_objects::competitions::{
+        name::CompetitionName, rules::CompetitionRules, standing::Standing,
+    },
 };
 use derive_more::{Display, From};
 use uuid::Uuid;
@@ -8,6 +11,7 @@ use uuid::Uuid;
 #[derive(Debug, PartialEq, Eq, From, Display)]
 pub struct CompetitionId(Uuid);
 
+#[derive(Debug, PartialEq, Eq, From, Display)]
 pub enum CompetitionType {
     League,
     Knockout,
@@ -16,19 +20,83 @@ pub enum CompetitionType {
     Custom,
 }
 
-pub struct CompetitionRules {
-    pub points_for_win: u8,
-    pub points_for_draw: u8,
-    pub promotion_slots: u8,
-    pub relegation_slots: u8,
+#[derive(Debug, PartialEq, Eq)]
+pub struct Competition {
+    id: CompetitionId,
+    name: CompetitionName,
+    rules: CompetitionRules,
+    format: CompetitionType,
+    fixtures: Vec<FixtureId>,
+    participants: Vec<ClubId>,
+    standings: Option<Standing>,
 }
 
-pub struct Competition {
-    pub id: CompetitionId,
-    pub name: String,
-    pub rules: CompetitionRules,
-    pub format: CompetitionType,
-    pub fixtures: Vec<FixtureId>,
-    pub participants: Vec<ClubId>,
-    pub standings: Option<Standing>,
+impl Competition {
+    pub fn new(
+        name: CompetitionName,
+        rules: CompetitionRules,
+        format: CompetitionType,
+        fixtures: Vec<FixtureId>,
+        participants: Vec<ClubId>,
+        standings: Option<Standing>,
+    ) -> Result<Self, DomainError> {
+        if participants.len() < 2 && format != CompetitionType::Friendly {
+            return Err(DomainError::Validation(
+                "Non-friendly competition must have at least two participants.".to_string(),
+            ));
+        }
+
+        Ok(Self {
+            id: CompetitionId::from(Uuid::new_v4()),
+            name,
+            rules,
+            format,
+            fixtures,
+            participants,
+            standings,
+        })
+    }
+
+    pub fn id(&self) -> &CompetitionId {
+        &self.id
+    }
+
+    pub fn name(&self) -> &CompetitionName {
+        &self.name
+    }
+
+    pub fn rules(&self) -> &CompetitionRules {
+        &self.rules
+    }
+
+    pub fn format(&self) -> &CompetitionType {
+        &self.format
+    }
+
+    pub fn fixtures(&self) -> &[FixtureId] {
+        &self.fixtures
+    }
+
+    pub fn participants(&self) -> &[ClubId] {
+        &self.participants
+    }
+
+    pub fn standings(&self) -> &Option<Standing> {
+        &self.standings
+    }
+
+    pub fn add_participant(&mut self, club_id: ClubId) {
+        if !self.participants.contains(&club_id) {
+            self.participants.push(club_id);
+        }
+    }
+
+    pub fn remove_participant(&mut self, club_id: &ClubId) {
+        self.participants
+            .retain(|participant| participant != club_id);
+    }
+
+    pub fn update_standings(&mut self, new_standings: Standing) {
+        self.standings = Some(new_standings);
+    }
 }
