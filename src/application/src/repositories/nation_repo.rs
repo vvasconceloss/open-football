57dfb08ffd427d9e765d0a885e337a5066901ad9
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-21 19:41:03 +0100

    feat(app): create a repository for nation
    - use a trait to create a repository with basic functions for nation (save, list, and find)

---
+++ src/application/src/repositories/nation_repo.rs
@@ -0,0 +1,8 @@
+use crate::errors::ApplicationError;
+use domain::entities::nation::{Nation, NationId};
+
+pub trait NationRepository {
+    fn save(&self, nation: Nation) -> Result<(), ApplicationError>;
+    fn find_by_id(&self, id: &NationId) -> Result<Option<Nation>, ApplicationError>;
+    fn list_all(&self) -> Result<Vec<Nation>, ApplicationError>;
+}
